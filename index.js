673be32c7ff105e7bd76ace6fcfeca7a750be098
lenkatlapakova <120376314+lenkatlapakova@users.noreply.github.com>
2023-11-19 16:38:20 +0100

    Update index.js
    nahranin editu

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 console.log('funguju!');
+function parseDate(dateString) {
+    const casti = dateString.split('.');
+    const den = parseInt(casti[0], 10);
+    const mesic = parseInt(casti[1], 10);
+    const rok = parseInt(casti[2], 10);
+    return { den, mesic, rok }; 
+}
+
+function promptDatum() {
+    const zadejDatum = prompt('Zadej datum ve formátu DD.MM.YYYY:');
+    const parsedDate = parseDate(zadejDatum);
+    displayDate(parsedDate);
+}
+
+function displayDate({ den, mesic, rok }) {
+    document.body.innerHTML += `<p>den ${den} </p>
+    <p>mesic  ${mesic}</p>
+    <p>rok  ${rok}</p>`;
+}
+
+promptDatum();
+
+
+
